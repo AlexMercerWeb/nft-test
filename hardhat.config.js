49692fea26e1fe2487da744e2d45a2ddf19897b1
--global <alex@webmercer.com>
2022-05-25 21:17:17 -0400

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,35 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("./scripts/deploy.js");
+require("./scripts/mint.js");
+
+const { RINKEBY_KEY, MUMBAI_KEY, PRIVATE_KEY } = process.env;
+
+module.exports = {
+    solidity: {
+	compilers: [
+	    {
+		version: "0.8.0",
+	    },
+	    {
+		version: "0.8.1",
+	    }
+	],
+    },
+    defaultNetwork: "rinkeby",
+    networks: {
+	hardhat: {},
+	rinkeby: {
+	    url: `https://ethereum-rinkeby--rpc.datahub.figment.io/apikey/${RINKEBY_KEY}`,
+	    accounts: [`0x${PRIVATE_KEY}`]
+	},
+	mumbai: {
+	    chainId: 80001,
+	    url: `https://polygon-mumbai.g.alchemy.com/v2/${MUMBAI_KEY}`,
+	    accounts: [`0x${PRIVATE_KEY}`]
+	},
+    },
+};
